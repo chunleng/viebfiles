587ad26f28292a830490bc2c4854fb81b07caafa
Lim Chun Leng <lim@logly.co.jp>
2022-07-20 09:38:32 +0900

    add vieb scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ set -eu
 
 APP_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
-mkdir -p ~/Library/Application\ Support/Vieb/colors
-ln -sfn $APP_PATH/viebrc ~/Library/Application\ Support/Vieb/viebrc
-ln -sfn $APP_PATH/colors/custom.css ~/Library/Application\ Support/Vieb/colors/custom.css
+mkdir -p ~/.vieb/colors
+ln -sfn $APP_PATH/viebrc ~/.vieb/viebrc
+ln -sfn $APP_PATH/colors/custom.css ~/.vieb/colors/custom.css
+ln -sfn $APP_PATH/scripts ~/.vieb/scripts
