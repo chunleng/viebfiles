620e6a8963f2080753fc65cdea036a5a4548987e
Lim Chun Leng <limchunleng@gmail.com>
2022-12-14 12:14:24 +0900

    Implement viebrc local

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,3 +9,5 @@ ln -sfn $APP_PATH/viebrc ~/.vieb/viebrc
 ln -sfn $APP_PATH/colors/custom.css ~/.vieb/colors/custom.css
 ln -sfn $APP_PATH/scripts ~/.vieb/scripts
 ln -sfn $APP_PATH/userstyle/ ~/.vieb/userstyle
+
+touch ~/.viebrc_local
