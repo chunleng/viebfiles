eaf078fa570f5e999c69f7a8379fd661a493ffa2
Lim Chun Leng <limchunleng@gmail.com>
2023-02-15 12:06:23 +0900

    fix: bootstrap bug for setting viebrc local

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,4 +10,4 @@ ln -sfn $APP_PATH/colors/custom.css ~/.vieb/colors/custom.css
 ln -sfn $APP_PATH/scripts ~/.vieb/scripts
 ln -sfn $APP_PATH/userstyle/ ~/.vieb/userstyle
 
-if [[ ! -e ~/.viebrc_local. ]]; then echo '" Local only settings here' > ~/.viebrc_local; fi
+if [[ ! -e ~/.viebrc_local ]]; then echo '" Local only settings here' > ~/.viebrc_local; fi
